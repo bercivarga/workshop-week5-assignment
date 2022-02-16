dda2e4ea9a926c873bc855991107c6bdca2155f6
David van der Vlugt <david@spotr.ai>
2022-02-16 22:29:00 +0100

    Pagination

--- fetch.js
+++ fetch.js
@@ -3,30 +3,57 @@
 //URL variables
 const API_URL_BASE = "https://pokeapi.co/api/v2/pokemon"
 let limit = 100
-let offset = 0
-let API_URL_COMPILED = `${API_URL_BASE}?limit=${limit}&offset=${offset}`
-const promise = fetch(API_URL_COMPILED)
-console.log(API_URL_COMPILED)
+let objectCounter = 100
+let resultCount = 0
+let total = 0
+let urlNext = ''
+let urlPrev = ''
+
 
 //Fetching DOM-elements
-let pokemonList = document.getElementsByClassName('pokemonList')
-pokemonList = pokemonList[0] //
-console.log(pokemonList)
+let pokemonList = document.querySelector('.pokemonList')
+let filterPrevious = document.querySelector('.previous')
+let filterNext = document.querySelector('.next')
+let filterCounter = document.querySelector('.counter')
 
-// Fetching data /////////////////////////////////////////////////
-promise
+
+
+// functions /////////////////////////////////////////////////
+
+//Fetching data
+function retrievePokemon(Url) {
+    pokemonList.innerHTML = ''
+    fetch(Url)
     .then(function(response) {
         if (response.status != '200') {
             throw new Error('Bad response')
         }
-        const data = response.json()
+        let data = response.json()
         return data
     })
     .then(function(data) {
+        console.log(data)
+        total = data.count
+        resultCount = data.results.length
+        console.log(resultCount)
+        filterCounter.innerHTML = `${objectCounter} of ${total}`
+        urlNext = data.next
+        urlPrev = data.previous
         for (let i = 0; i < data.results.length; i++) {
-            console.log(data.results[i].name)
-            pokemonList.innerHTML += `<div>${data.results[i].name}</div>`
+            pokemonList.innerHTML += `<div class='pokemon'>${data.results[i].name}</div>`
         }
+
+        if (data.previous !== null) {
+            filterPrevious.innerHTML = "<button class='filter previous'>Previous</button>"
+        } else {
+            filterPrevious.innerHTML = ''
+        }
+        if (data.next !== null) {
+            filterNext.innerHTML = "<button class='filter next'>Next</button>"
+        } else {
+            filterNext.innerHTML = ''
+        }
+        
     })
     .catch(function(error) {
         console.error(error)
@@ -34,8 +61,35 @@ promise
         pokemonList.innerHTML = `
             <h1>Bad request</h1>`
     }) 
+}
+
+//Next page
+function nextPage() {
+    retrievePokemon(urlNext)
+    console.log(objectCounter)
+    console.log(resultCount)
+    objectCounter += resultCount
+}
+
+//Previous page
+function previousPage () {
+    retrievePokemon(urlPrev)
+    objectCounter -= resultCount
+}
 
 
-// pokemonList.innerHTML = `
-// <img src=${data.icon_url}></img>
-// <h1>${value}</h1>`
+
+//Setting filter button event listeners
+filterNext.addEventListener(
+    'click',
+    function() {nextPage()}
+)
+
+filterPrevious.addEventListener(
+    'click',
+    function() {previousPage()}
+)
+
+// Initial calls /////////////////////////////////////////////////
+
+retrievePokemon(`${API_URL_BASE}?limit=${limit}`)
